d5232c3a06c4f8635abc06ded26601b9ce080a54
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-07-13 12:54:57 +0100

    Completed logpdf for OU process

--- src/RDE.jl
+++ src/RDE.jl
@@ -3,12 +3,16 @@ module RDE
 using Brownian
 using Distributions
 
+import Distributions: logpdf
+
 export
   OrnsteinUhlenbeck,
+  OU,
   ito!,
   ito,
   invito!,
-  invito
+  invito,
+  logpdf
 
 include(joinpath("univariate", "OrnsteinUhlenbeck.jl"))
 
--- src/univariate/OrnsteinUhlenbeck.jl
+++ src/univariate/OrnsteinUhlenbeck.jl
@@ -14,10 +14,12 @@ end
 OrnsteinUhlenbeck(l::Float64, x::Union(BrownianMotion, FBM)) = OrnsteinUhlenbeck(l, 1., x)
 OrnsteinUhlenbeck(x::Union(BrownianMotion, FBM)) = OrnsteinUhlenbeck(1., 1., x)
 
+const OU = OrnsteinUhlenbeck
+
 ### Ito map from rough path increment dx to the next iteration of the solution given the previous iteration y of the
 ### solution
 function ito(y::Float64, dx::Float64, p::OrnsteinUhlenbeck)
-  ld::Float64 = l/(p.x.n-1)
+  ld::Float64 = p.l/(p.x.n-1)
   expmld::Float64 = exp(-ld)
   y*expmld+(p.σ)*dx*(1-expmld)/ld  
 end
@@ -26,7 +28,7 @@ end
 function ito!(y::Vector{Float64}, dx::Vector{Float64}, p::OrnsteinUhlenbeck)
   y[1] = 0.
   for i = 1:p.x.n-1
-    y[i+i] = ito(y[i], dx[i], p)
+    y[i+1] = ito(y[i], dx[i], p)
   end
   y
 end
@@ -35,16 +37,17 @@ ito(dx::Vector{Float64}, p::OrnsteinUhlenbeck) = ito!(Array(Float64, p.x.n), dx,
 
 ### Inverse Ito map takes two successive values of the solution vecto and returns a rough path increment dx
 function invito(y0::Float64, y1::Float64, p::OrnsteinUhlenbeck)
-  ld::Float64 = l/(p.x.n-1)
+  ld::Float64 = p.l/(p.x.n-1)
   expmld::Float64 = exp(-ld)
   ld*(y1-y0*expmld)/(p.σ*(1-expmld))
 end
 
 function invito!(dx::Vector{Float64}, y::Vector{Float64}, p::OrnsteinUhlenbeck)
-  ld::Float64 = l/(p.n-1)
+  pnmone::Int64 = p.x.n-1
+  ld::Float64 = p.l/(pnmone)
   expmld::Float64 = exp(-ld)
 
-  for i = 1:p.n
+  for i = 1:pnmone
     dx[i] = ld*(y[i+1]-y[i]*expmld)/(p.σ*(1-expmld))
   end
 
@@ -52,3 +55,10 @@ function invito!(dx::Vector{Float64}, y::Vector{Float64}, p::OrnsteinUhlenbeck)
 end
 
 invito(y::Vector{Float64}, p::OrnsteinUhlenbeck) = invito!(Array(Float64, p.x.n-1), y, p)
+
+# Log-pdf of increments of linearly interpolated rough path x
+function logpdf(y::Vector{Float64}, p::OrnsteinUhlenbeck)
+  pnmone::Int64 = p.x.n-1
+  ld::Float64 = p.l/pnmone
+  logpdf(MvNormal(autocov(convert(FGN, p.x), pnmone)), invito(y, p))/pnmone+log(ld/(p.σ*(1-exp(-ld))))
+end
