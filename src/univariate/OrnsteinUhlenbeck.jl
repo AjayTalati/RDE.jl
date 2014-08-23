e197ecc5be02d1641fed8a5df2ed114014f225ce
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-08-23 15:24:58 +0100

    Improved interface of logpdf for increments of linearly interpolated rough path x

--- src/univariate/OrnsteinUhlenbeck.jl
+++ src/univariate/OrnsteinUhlenbeck.jl
@@ -1,6 +1,6 @@
 ### Ornstein-Uhlenbeck process: dy_t = -λ*y_t*dt+σ*dx_t, y_0 = 0
 immutable OrnsteinUhlenbeck <: ContinuousUnivariateStochasticProcess
-  λ::Float64 # drift parameter
+  λ::Float64 # drift parameter (also known as inverse relaxation time)
   σ::Float64 # diffusion parameter
   x::Union(BrownianMotion, FBM) # rough path, which is either Brownian motion or fractional Brownian motion
 
@@ -33,31 +33,31 @@ rand(p::OrnsteinUhlenbeck, p0::Float64) = rand!(Array(Float64, p.x.n-1), p, p0)
 
 ### Ito map from rough path increment dx to the next iteration of the solution given the previous iteration y of the
 ### solution
-function ito(y::Float64, dx::Float64, p::OrnsteinUhlenbeck)
+function ito(p::OrnsteinUhlenbeck, y::Float64, dx::Float64)
   λδ::Float64 = p.λ*p.x.t[end]/(p.x.n-1)
   expmλδ::Float64 = exp(-λδ)
   y*expmλδ+(p.σ)*dx*(1-expmλδ)/λδ  
 end
 
 ### Ito map which takes a vector of increments dx as input, i.e. a vector of fractional Guassian noise samples
-function ito!(y::Vector{Float64}, dx::Vector{Float64}, p::OrnsteinUhlenbeck)
-  y[1] = ito(0., dx[1], p)
+function ito!(y::Vector{Float64}, p::OrnsteinUhlenbeck, dx::Vector{Float64})
+  y[1] = ito(p, 0., dx[1])
   for i = 2:p.x.n-1
-    y[i] = ito(y[i-1], dx[i], p)
+    y[i] = ito(p, y[i-1], dx[i])
   end
   y
 end
 
-ito(dx::Vector{Float64}, p::OrnsteinUhlenbeck) = ito!(Array(Float64, p.x.n-1), dx, p)
+ito(p::OrnsteinUhlenbeck, dx::Vector{Float64}) = ito!(Array(Float64, p.x.n-1), p, dx)
 
 ### Inverse Ito map takes two successive values of the solution vector and returns a rough path increment dx
-function invito(y0::Float64, y1::Float64, p::OrnsteinUhlenbeck)
+function invito(p::OrnsteinUhlenbeck, y0::Float64, y1::Float64)
   λδ::Float64 = p.λ*p.x.t[end]/(p.x.n-1)
   expmλδ::Float64 = exp(-λδ)
   λδ*(y1-y0*expmλδ)/(p.σ*(1-expmλδ))
 end
 
-function invito!(dx::Vector{Float64}, y::Vector{Float64}, p::OrnsteinUhlenbeck)
+function invito!(dx::Vector{Float64}, p::OrnsteinUhlenbeck, y::Vector{Float64})
   pnmone::Int64 = p.x.n-1
   λδ::Float64 = p.λ*p.x.t[end]/pnmone
   expmλδ::Float64 = exp(-λδ)
@@ -70,38 +70,52 @@ function invito!(dx::Vector{Float64}, y::Vector{Float64}, p::OrnsteinUhlenbeck)
   dx
 end
 
-invito(y::Vector{Float64}, p::OrnsteinUhlenbeck) = invito!(Array(Float64, p.x.n-1), y, p)
+invito(p::OrnsteinUhlenbeck, y::Vector{Float64}) = invito!(Array(Float64, p.x.n-1), p, y)
+
+# Auxiliary functions that compute the quadratic form of log-pdf of increments of linearly interpolated rough path x
+function quad_ou(p::OrnsteinUhlenbeck, yPy::Float64, lPl::Float64, yPl::Float64)
+  expmλδ::Float64 = exp(-p.λ*p.x.t[end]/(p.x.n-1))  
+  yPy+abs2(expmλδ)*lPl-2*expmλδ*yPl
+end
 
 # Log-pdf of increments of linearly interpolated rough path x
-function logpdf(y::Vector{Float64}, p::OrnsteinUhlenbeck; C::Matrix{Float64}=autocov(convert(FGN, p.x), p.x.n-1))
+function logpdf(p::OrnsteinUhlenbeck, q::Float64, logdetC::Float64)
+  δ::Float64 = p.x.t[end]/(p.x.n-1)
+  λδ::Float64 = p.λ*δ
+  invφ::Float64 = abs2(λδ/(p.σ*(1-exp(-λδ))))
+  0.5*(-δ*(invφ*q+logdetC)-log(2*pi)+log(invφ))
+end
+
+logpdf(p::OrnsteinUhlenbeck, yPy::Float64, lPl::Float64, yPl::Float64, logdetC::Float64) =
+  logpdf(p, quad_ou(p, yPy, lPl, yPl), logdetC)
+
+function logpdf(p::OrnsteinUhlenbeck, y::Vector{Float64}, C::Matrix{Float64}=autocov(convert(FGN, p.x), p.x.n-1))
   δ::Float64 = p.x.t[end]/(p.x.n-1)
   λδ::Float64 = p.λ*δ
   δ*logpdf(MvNormal(C), invito(y, p))+log(λδ/(p.σ*(1-exp(-λδ))))
 end
 
-function logpdf(y::Vector{Float64}, p::OrnsteinUhlenbeck, logdetC::Float64, yPy::Float64, lPl::Float64, yPl::Float64)
-  δ::Float64 = p.x.t[end]/(p.x.n-1)
-  λδ::Float64 = p.λ*δ
-  expmλδ::Float64 = exp(-λδ)
-  phiinv::Float64 = (λδ/(p.σ*(1-expmλδ)))^2
-  0.5*(-δ*(phiinv*(yPy+exp(-2*λδ)*lPl-2*expmλδ*yPl)+logdetC)-log(2*pi)+log(phiinv))
-end
-
 # Approximate MLE estimator of drift parameter of OU process with BM noise
 function approx_mle_ou_drift(y::Vector{Float64}, x::BrownianMotion, y0::Float64=0.)
-  ly::Vector{Float64} = [y0, y[1:end-1]]
-  log(dot(ly, ly)/dot(ly, y))*(x.n-1)/x.t[end]
+  l::Vector{Float64} = [y0, y[1:end-1]]
+  log(dot(l, l)/dot(l, y))*(x.n-1)/x.t[end]
 end
 
 # Approximate MLE estimator of drift parameter of OU process with FBM noise
 function approx_mle_ou_drift(y::Vector{Float64}, x::FBM, y0::Float64=0.)
-  ly::Vector{Float64} = [y0, y[1:end-1]]
+  l::Vector{Float64} = [y0, y[1:end-1]]
   pnmone::Int64 = x.n-1
   P::Matrix{Float64} = inv(autocov(convert(FGN, x), pnmone))
-  Ply::Vector{Float64} = P*ly
-  log(dot(ly, Ply)/dot(y, Ply))*pnmone/x.t[end]
+  Pl::Vector{Float64} = P*l
+  log(dot(l, Pl)/dot(y, Pl))*pnmone/x.t[end]
 end
 
-#approx_mle_ou_diffusion
+# Approximate MLE estimator of diffusion parameter of OU process with BM noise
+#function approx_mle_ou_diffusion(y::Vector{Float64}, x::BrownianMotion, l::Float64, y0::Float64=0.)
+#end
+
+# Approximate MLE estimator of diffusion parameter of OU process with FBM noise
+#function approx_mle_ou_diffusion(y::Vector{Float64}, x::FBM, l::Float64, y0::Float64=0.)
+#end
 
 #approx_mle_ou
