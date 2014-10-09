07dfd02f558132fcdb7574edd42dd709be31d140
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-10-09 22:38:50 +0100

    Cleaned up interface and updated existing test

--- src/RDE.jl
+++ src/RDE.jl
@@ -4,25 +4,21 @@ using Brownian
 using Distributions
 
 import Base: convert, rand!, rand
+import Brownian: autocov
+import Distributions: loglikelihood, fit_mle
 
 export
-  OrnsteinUhlenbeck,
   OU,
+  FOU,
   rand!,
   rand,
   ito!,
   ito,
   invito!,
   invito,
-  exact_loglik_ou,
-  approx_loglik_ou,
-  exact_mle_ou_drift,
-  approx_mle_ou_drift,
-  exact_mle_ou_diffusion,
-  approx_mle_ou_diffusion,
-  exact_mle_ou,
-  approx_mle_ou
+  loglikelihood,
+  fit_mle
 
-include(joinpath("univariate", "OrnsteinUhlenbeck.jl"))
+include(joinpath("univariate", "OU.jl"))
 
 end
--- test/ou_bm.jl
+++ test/ou_bm.jl
@@ -13,14 +13,24 @@ ou = OU(λ, σ, bm);
 # Generate data from the OU process via exact simulation using Gillespie's algorithm
 data = rand(ou, 2.);
 
-# Compute exact MLE estimator of of (λ, σ) parameters of OU-BM
-exact_mle_ou(bm, data);
+# Compare exact loglikelihood of OU based on the explicit formula and on the logpdf from Distributions
+x = rand(bm.n-1)
+y = loglikelihood(ou, x; accuracy=:exact, method=:explicit)
+z = loglikelihood(ou, x; accuracy=:exact, method=:pdf)
+@test_approx_eq_eps y z 1e-8
 
-# Compute approximate MLE estimator of of (λ, σ) parameters of OU-BM
-approx_mle_ou(bm, data);
+# Compute exact MLE estimator of of (λ, σ) parameters of OU
+fit_mle(OU(bm), data, accuracy=:exact);
+
+# Compute approximate MLE estimator of of (λ, σ) parameters of OU
+fit_mle(OU(bm), data, accuracy=:approx);
 
 # Compare exact MLE estimate of λ via BM with approximate MLE estimate of λ via FBM
-@test_approx_eq_eps exact_mle_ou_drift(bm, data) approx_mle_ou_drift(convert(FBM, bm), data) 1e-8
+x = fit_mle(OU(bm), data, parameter=:drift, accuracy=:exact);
+y = fit_mle(FOU(bm), data, parameter=:drift);
+@test_approx_eq_eps x y 1e-8
 
 # Compare approximate MLE estimate of σ via BM with approximate MLE estimate of σ via FBM
-@test_approx_eq_eps approx_mle_ou_diffusion(bm, data) approx_mle_ou_diffusion(convert(FBM, bm), data) 1e-8
+x = fit_mle(OU(bm), data, parameter=:diffusion, accuracy=:approx);
+y = fit_mle(FOU(bm), data, parameter=:diffusion);
+@test_approx_eq_eps x y 1e-8
